9d7a391cd5c3b38e6e5bba751fc0b7dd0c45d0a0
Shriyansh Kasoudhan <38802997+shriyansh1193@users.noreply.github.com>
2021-04-06 22:46:33 +0530

    D

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          Hello There!
         </p>
         <a
           className="App-link"
